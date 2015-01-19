3ed89f225add69283bcab9ece26c611450d695f8
Kangie Chan <aries0420@gmail.com>
2015-01-18 23:24:51 -0800

    Simple app for downloading and displaying web content.

--- Downloading Web Content/ViewController.swift
+++ Downloading Web Content/ViewController.swift
@@ -10,9 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var webView: UIWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let url = NSURL(string: "http://www.shermanmeng.com")
+        
+        let task = NSURLSession.sharedSession().dataTaskWithURL(url!) {
+            (data, response, error) in
+            if error == nil {
+                
+                var urlContent = NSString(data: data, encoding: NSUTF8StringEncoding)
+                
+//                println(urlContent)
+                
+                dispatch_async(dispatch_get_main_queue()) {
+                    self.webView.loadHTMLString(urlContent!, baseURL: nil)
+                }
+            }
+        }
+        
+        task.resume()
     }
 
     override func didReceiveMemoryWarning() {
